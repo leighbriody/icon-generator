bae52ed7f8d97c0e549453eec2f17f5fa1ea2170
leigh <lbriody10@gmail.com>
2023-05-01 23:02:35 +0100

    UI changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,11 +15,7 @@ export function Header() {
     <nav className="border-gray-200 bg-white dark:bg-gray-900">
       <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
         <div className="flex items-center">
-          {/* <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            class="mr-3 h-8"
-            alt="Flowbite Logo"
-          /> */}
+
           <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
             <PrimaryLink href={"/"}>Icon Generator</PrimaryLink>
           </span>
